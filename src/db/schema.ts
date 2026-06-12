34451dffecfc97f365d03a026dce971137b72f01
Tynahan <nusilite@gmail.com>
2026-06-12 17:41:13 +0900

    chore: fix build error

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,15 @@
-import { pgTable, unique, integer, varchar, date, bigint } from "drizzle-orm/pg-core"
-
-export const app_user = pgTable("app_user", {
-    id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
-})
-
-export const memory = pgTable(
-    "memory",
-    {
-        id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
-        date: date().notNull,
-        app_user_id: bigint({ mode: "bigint" }),
-    },
-    (table) => [unique("users_email_unique").on(table.email)],
-)
+// import { pgTable, unique, integer, varchar, date, bigint } from "drizzle-orm/pg-core"
+//
+// export const app_user = pgTable("app_user", {
+//     id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
+// })
+//
+// export const memory = pgTable(
+//     "memory",
+//     {
+//         id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
+//         date: date().notNull,
+//         app_user_id: bigint({ mode: "bigint" }),
+//     },
+//     (table) => [unique("users_email_unique").on(table.email)],
+// )
