7bc41233e566058e8efdaf0b523ed168a9108f15
Tynahan <nusilite@gmail.com>
2026-06-13 15:55:26 +0900

    feat: add image upload download

--- src/features/debug/router/debug-router.ts
+++ src/features/debug/router/debug-router.ts
@@ -3,6 +3,7 @@ import express, { Router } from "express"
 const router: Router = express.Router()
 
 router.get("/", async (_req, res) => {
+    console.log("---- debug called, hello world")
     res.status(200).send("---- hello world")
 })
 
--- src/features/image/router/image-router.ts
+++ src/features/image/router/image-router.ts
@@ -1,8 +1,29 @@
 import express, { type Router } from "express"
+import { readdirSync, readFileSync } from "fs"
+import multer from "multer"
+const upload = multer({ dest: "./uploads/" })
 
 const imageRouter: Router = express.Router()
 
-imageRouter.post("/", async (_req, res) => {
+imageRouter.get("/", async (_req, res) => {
+    const files = readdirSync("uploads")
+    const sampleImage = readFileSync(`uploads/${files[0]}`)
+    res.contentType("image/jpeg")
+    res.status(200).send(sampleImage)
+})
+
+const uploadMiddleware = upload.single("image")
+imageRouter.post("/", uploadMiddleware, async (req, res) => {
+    const file = req.file
+    if (!file) {
+        res.status(400).json({ message: "---- post memory payload invalid" })
+        return
+    }
+
+    const image = file
+    const { date, front_message, rear_message } = req.body
+    console.log({ image, date, front_message, rear_message })
+    console.log("---- image posted")
     res.status(200).send("---- post image response placeholder")
 })
 
