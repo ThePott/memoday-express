9a2d9d596d5ac12a1d9169306f57a86a12dfcf09
Tynahan <nusilite@gmail.com>
2026-06-14 15:14:27 +0900

    schema: update date mode

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 import { DATABASE_URL } from "./src/shared/config/env-var.js"
+
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
